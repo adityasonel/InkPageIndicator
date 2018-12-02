8a50e3e9bc3d7f7468514fe1dec24287526fb30c
Aditya Sonel <aditya.sonel@outlook.com>
2018-12-02 16:23:06 +0530

    Sample Added

--- app/src/main/java/com/adityasonel/inkpageindicator/MainActivity.java
+++ app/src/main/java/com/adityasonel/inkpageindicator/MainActivity.java
@@ -1,7 +1,18 @@
 package com.adityasonel.inkpageindicator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.animation.ArgbEvaluator;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.adityasonel.inkpage_indicator.InkPageIndicator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        AppAdapter appAdapter = new AppAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(appAdapter);
+
+        InkPageIndicator pageIndicator = findViewById(R.id.page_indicator);
+        pageIndicator.setViewPager(viewPager);
+
+        ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+
+        final int color = ContextCompat.getColor(this, android.R.color.holo_orange_light);
+        final int color2 = ContextCompat.getColor(this, android.R.color.holo_green_light);
+        final int color3 = ContextCompat.getColor(this, android.R.color.holo_blue_light);
+        final int[] iArr = new int[]{color, color2, color3};
+
+        TextView main = findViewById(R.id.tv_main);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                int valueOf = iArr[position];
+                if (position != 2) {
+                    position++;
+                }
+                position = (int) argbEvaluator.evaluate(positionOffset, valueOf, iArr[position]);
+                main.setTextColor(position);
+            }
+
+            @Override
+            public void onPageSelected(int position) { }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    class AppAdapter extends FragmentPagerAdapter {
+        AppAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return MainFragment.newInstance();
+            } else if (position == 1) {
+                return MainFragment.newInstance();
+            } else if (position == 2) {
+                return MainFragment.newInstance();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
     }
 }
---
+++ app/src/main/java/com/adityasonel/inkpageindicator/MainFragment.java
@@ -0,0 +1,32 @@
+package com.adityasonel.inkpageindicator;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() { }
+
+    static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
